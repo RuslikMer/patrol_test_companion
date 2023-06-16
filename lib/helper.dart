dcce85694c4cc99f7e2bc324549ae5f4aa35a75b
rus <rmerikanov@local>
2023-06-16 12:59:33 +0400

    fix 6

---
+++ lib/helper.dart
@@ -0,0 +1,6 @@
+
+library helper;
+
+export 'src/helper/globEnv.dart';
+export 'src/helper/helpers.dart';
+export 'src/helper/qa_api.dart';
---
+++ lib/pages.dart
@@ -0,0 +1,15 @@
+
+library pages;
+
+export 'src/pages/base.dart';
+export 'src/pages/cart.dart';
+export 'src/pages/catalog.dart';
+export 'src/pages/checkout.dart';
+export 'src/pages/home.dart';
+export 'src/pages/login.dart';
+export 'src/pages/menu.dart';
+export 'src/pages/nav_bar.dart';
+export 'src/pages/product.dart';
+export 'src/pages/profile.dart';
+export 'src/pages/search.dart';
+export 'src/pages/wishlist.dart';
